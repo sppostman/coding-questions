fa7bd8bfb9ab375351f7950c1f185b619049028a
sppostman <67599966+sppostman@users.noreply.github.com>
2023-05-14 00:10:01 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (31.84%) - LeetSync

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     static map<pair<int,int>, int> dp;
     int uniquePaths(int m, int n) {
-        if(dp.count(make_pair(m,n)))
-            return dp[make_pair(m,n)];
+        pair<int,int> curr = make_pair(m,n);
+        if(dp.count(curr))
+            return dp[curr];
         if(m==1 || n==1){
             return 1;
         }
         int res = uniquePaths(m-1, n) + uniquePaths(m, n-1);
-        dp[make_pair(m,n)] = res;
+        dp[curr] = res;
         return res;
     }
 };
