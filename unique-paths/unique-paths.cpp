d6e5d7063c470c5bb86f9f7d2257717a29845fbb
sppostman <67599966+sppostman@users.noreply.github.com>
2023-05-28 11:51:52 +0530

    Time: 3 ms (36.78%) | Memory: 6 MB (76.01%) - LeetSync

--- unique-paths/unique-paths.cpp
+++ unique-paths/unique-paths.cpp
@@ -2,15 +2,24 @@ class Solution {
 public:
     static map<pair<int,int>, int> dp;
     int uniquePaths(int x, int y) {
-        // Optimal nCr
         int n = x+y-2;
-        int r = x>y ? y-1 : x-1;    // Ensure least combinations
-        double res = 1;
-
-        for(int i=1; i<=r; i++){
-            res = (res * (n-r+i))/i;
+        int r = x>y ? y-1 : x-1;
+        long res=1;
+        for(int i=1;i<=r;i++){
+            res = (n-i+1)*res / i;
         }
         return (int)res;
+
+        // Optimal nCr
+        // int n = x+y-2;
+        // int r = x>y ? y-1 : x-1;    // Ensure least combinations
+        // double res = 1;
+
+        // for(int i=1; i<=r; i++){
+        //     res = (res * (n-r+i))/i;
+        // }
+        // return (int)res;
+        
         // pair<int,int> curr = make_pair(m,n);
         // if(dp.count(curr))
         //     return dp[curr];
