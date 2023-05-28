3b217b87c9a2cbd7eebf72ce2ddea48ae3fb46e0
sppostman <67599966+sppostman@users.noreply.github.com>
2023-05-28 13:21:59 +0530

    Time: 294 ms (85.91%) | Memory: 46.8 MB (83.81%) - LeetSync

--- reverse-pairs/reverse-pairs.cpp
+++ reverse-pairs/reverse-pairs.cpp
@@ -3,25 +3,25 @@ public:
     int merge(vector<int>&nums, vector<int>&temp, int l, int mid, int r){
         if(l==r)
             return 0;
-        int i=l, j=mid+1;
+        int i=l, j=mid;
         int count = 0;
 
-        for(i=l; i<=mid; i++){
+        for(i=l; i<mid; i++){
             while(j<=r && nums[i] > 2LL * (long)nums[j])
                 j++;
-            count += (j-(mid+1));
+            count += (j-(mid));
         }
 
-        i=l,j=mid+1;
+        i=l,j=mid;
         int k=l;
-        while(i<=mid && j<=r){
+        while(i<mid && j<=r){
             if(nums[i] <= nums[j]){
                 temp[k++] = nums[i++];
             } else {
                 temp[k++] = nums[j++];
             }
         }
-        while(i <= mid)
+        while(i < mid)
             temp[k++] = nums[i++];
         while(j <= r)
             temp[k++] = nums[j++];
@@ -37,7 +37,7 @@ public:
         // printf("Mergesort %d...%d...%d (%d...%d...%d)\n",l,mid,r,nums[l],nums[mid],nums[r]);
         count += mergeSort(nums,temp,l,mid);
         count += mergeSort(nums,temp,mid+1,r);
-        count += merge(nums,temp,l,mid,r);
+        count += merge(nums,temp,l,mid+1,r);
         return count;
     }
     int reversePairs(vector<int>& nums) {
