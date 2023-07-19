4ecca33491d391c2f3f89743091e503fe7ecf6e8
sppostman <67599966+sppostman@users.noreply.github.com>
2023-07-19 11:55:10 +0530

    Time: 626 ms (13.47%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,3 +1,5 @@
-SELECT ifnull(max(salary), null) AS SecondHighestSalary
-FROM employee 
-WHERE salary < (SELECT MAX(salary) FROM employee);
+# SELECT ifnull(max(salary), null) AS SecondHighestSalary
+# FROM employee 
+# WHERE salary < (SELECT MAX(salary) FROM employee);
+
+select (select distinct salary from employee order by salary desc limit 1 offset 1 ) as SecondHighestSalary;
