dc642d946ca667f79583da61715f58fd7603a8e5
sppostman <67599966+sppostman@users.noreply.github.com>
2023-08-01 12:39:49 +0530

    Time: 25 ms (99.13%) | Memory: 12.6 MB (55.42%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
     void dfs(int i, int j, vector<vector<char>>& grid, int n, int m, vector<vector<bool>> &vis){
-        if(grid[i][j] == '0')   return;
-
         vis[i][j] = true;
         // printf("(%d,%d), ",i,j);
 
+        pair<int,int> combinations[] = {{i-1,j}, {i+1,j}, {i,j-1}, {i,j+1}};
         
-        if(i-1 > -1 && !vis[i-1][j])
-            dfs(i-1, j, grid, n, m,vis);
-        if(j-1 > -1 && !vis[i][j-1])
-            dfs(i, j-1, grid, n, m,vis);
-        if(i+1 < n && !vis[i+1][j])
-            dfs(i+1, j, grid, n, m,vis);
-        if(j+1 < m && !vis[i][j+1])
-            dfs(i, j+1, grid, n, m,vis);
+        for(auto i : combinations){
+            if(
+                i.first > -1 && i.first < n &&
+                i.second > -1 && i.second < m &&
+                grid[i.first][i.second]=='1' && !vis[i.first][i.second]
+            ){
+                // printf("Go to (%d,%d)\n", i.first,i.second);
+                dfs(i.first,i.second,grid,n,m,vis);
+            }
+        }
+        // if(i-1 > -1 && !vis[i-1][j])
+        //     dfs(i-1, j, grid, n, m,vis);
+        // if(j-1 > -1 && !vis[i][j-1])
+        //     dfs(i, j-1, grid, n, m,vis);
+        // if(i+1 < n && !vis[i+1][j])
+        //     dfs(i+1, j, grid, n, m,vis);
+        // if(j+1 < m && !vis[i][j+1])
+        //     dfs(i, j+1, grid, n, m,vis);
     }
     // Function to find the number of islands.
     int numIslands(vector<vector<char>>& grid) {
