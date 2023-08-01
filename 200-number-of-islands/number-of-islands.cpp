5e8395bae9ffce5d00fde52845ce6ec89e6e4f34
sppostman <67599966+sppostman@users.noreply.github.com>
2023-08-01 12:33:29 +0530

    Time: 30 ms (96.70%) | Memory: 12.4 MB (65.51%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void dfs(int i, int j, vector<vector<char>>& grid, int n, int m, vector<vector<bool>> &vis){
+        if(grid[i][j] == '0')   return;
+
+        vis[i][j] = true;
+        // printf("(%d,%d), ",i,j);
+
+        
+        if(i-1 > -1 && !vis[i-1][j])
+            dfs(i-1, j, grid, n, m,vis);
+        if(j-1 > -1 && !vis[i][j-1])
+            dfs(i, j-1, grid, n, m,vis);
+        if(i+1 < n && !vis[i+1][j])
+            dfs(i+1, j, grid, n, m,vis);
+        if(j+1 < m && !vis[i][j+1])
+            dfs(i, j+1, grid, n, m,vis);
+    }
+    // Function to find the number of islands.
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m=grid[0].size();
+        vector<vector<bool>> vis(n, vector<bool>(m,false));
+        
+        int islands=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
+                    islands++;
+                    // printf("Island starts (%d,%d), ",i,j);
+                    dfs(i, j, grid,n,m, vis);
+                    // printf("\n");
+                }
+            }
+        }
+        return islands;
+    }
+};
