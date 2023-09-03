a524d4c768e9be7312ce30fc60509b0709cc0a7e
sppostman <67599966+sppostman@users.noreply.github.com>
2023-09-03 16:13:06 +0530

    Time: 36 ms (25.46%) | Memory: 36.1 MB (21.78%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void combine(vector<int>& candidates, vector<int>curr, int idx, int target, vector<vector<int>> &result){
+        if(idx<0)
+            return;
+        if(target==0){
+            result.push_back(curr);
+            return;
+        }
+        // printf("Without %d\n", candidates[idx]);
+        combine(candidates, curr, idx-1, target, result);
+
+        // printf("With %d\n", candidates[idx]);
+        if(target >= candidates[idx]){
+            curr.push_back(candidates[idx]);
+            combine(candidates, curr, idx, target-candidates[idx], result);
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        // vector<int> curr;
+        vector<vector<int>> result;
+        combine(candidates, {}, candidates.size()-1, target, result);
+        return result;
+    }
+};
