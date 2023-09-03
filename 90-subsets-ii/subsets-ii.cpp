6defef3442caf97f446b2b49ffd499e929120e04
sppostman <67599966+sppostman@users.noreply.github.com>
2023-09-03 15:33:34 +0530

    Time: 11 ms (11.59%) | Memory: 17.5 MB (7.57%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -17,23 +17,36 @@ public:
         curr.push_back(nums[n]);
         subsets(nums, n-1, curr, result, taken);
     }
+
+    void subsetsSorted(
+        vector<int>& nums, int n, vector<int> curr,
+        vector<vector<int>> &result, set<vector<int>> &taken
+    ){
+        if(n<0){
+            // sort(curr.begin(), curr.end());
+            if(!taken.count(curr)){
+                result.push_back(curr);
+                taken.insert(curr);
+            }
+            return;
+        }
+
+        // while(n>0 && nums[n]==nums[n-1])
+        //     n--;
+        subsets(nums, n-1, curr, result, taken);
+
+        curr.push_back(nums[n]);
+        subsets(nums, n-1, curr, result, taken);
+    }
+
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         set<vector<int>> taken;
         vector<vector<int>> result;
         vector<int> curr;
-        subsets(nums, nums.size()-1, curr, result, taken);
+        // subsets(nums, nums.size()-1, curr, result, taken);
+
+        sort(nums.begin(), nums.end());
+        subsetsSorted(nums, nums.size()-1, curr, result, taken);
         return result;
-        // result.push_back({});
-        // for(int i=0;i<nums.size(); i++){
-        //     vector<int> curr;
-        //     for(int j=i; j<nums.size(); j++){
-        //         curr.push_back(nums[j]);
-        //         if(!taken.count(curr)){
-        //             result.push_back(curr);
-        //             taken.insert(curr);
-        //         }
-        //     }
-        // }
-        // return result;
     }
 };
