147aab76c67acae010d05db1aa3416fc2edf86b3
sppostman <67599966+sppostman@users.noreply.github.com>
2023-09-03 16:38:38 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (31.63%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void combine(vector<int>& candidates, vector<int>curr, int idx, int target, vector<vector<int>> &result){
+    void combine(vector<int>& candidates, vector<int> &curr, int idx, int target, vector<vector<int>> &result){
         if(idx<0)
             return;
         if(target==0){
@@ -12,12 +12,13 @@ public:
         if(target >= candidates[idx]){
             curr.push_back(candidates[idx]);
             combine(candidates, curr, idx, target-candidates[idx], result);
+            curr.pop_back();
         }
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        // vector<int> curr;
+        vector<int> curr;
         vector<vector<int>> result;
-        combine(candidates, {}, candidates.size()-1, target, result);
+        combine(candidates, curr, candidates.size()-1, target, result);
         return result;
     }
 };
