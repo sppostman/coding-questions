078562da4534c28ed93b3992b7c88dc55a178fa8
sppostman <67599966+sppostman@users.noreply.github.com>
2023-09-17 11:39:36 +0530

    Time: 5 ms (28.17%) | Memory: 8 MB (28.37%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,28 +1,52 @@
 class Solution {
 public:
-    void perm(
+    // void perm(
+    //     vector<int>& nums, vector<int> &curr,
+    //     set<int> &taken, vector<vector<int>> &result
+    // ){
+    //     if(taken.size() == nums.size()){
+    //         result.push_back(curr);
+    //         return;
+    //     }
+    //     for(int i=0;i<nums.size();i++){
+    //         if(!taken.count(nums[i])){
+    //             taken.insert(nums[i]);
+    //             curr.push_back(nums[i]);
+    //             perm(nums, curr, taken, result);
+    //             curr.pop_back();
+    //             taken.erase(nums[i]);
+    //         }
+    //     }
+    // }
+
+    void perm2(
         vector<int>& nums, vector<int> &curr,
-        set<int> &taken, vector<vector<int>> &result
+        vector<bool> &taken, vector<vector<int>> &result
     ){
-        if(taken.size() == nums.size()){
+        if(curr.size() == nums.size()){
             result.push_back(curr);
             return;
         }
         for(int i=0;i<nums.size();i++){
-            if(!taken.count(nums[i])){
-                taken.insert(nums[i]);
+            if(!taken[i]){
+                taken[i] = true;
                 curr.push_back(nums[i]);
-                perm(nums, curr, taken, result);
+                perm2(nums, curr, taken, result);
                 curr.pop_back();
-                taken.erase(nums[i]);
+                taken[i] = false;
             }
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> result;
-        set<int> taken;
         vector<int> curr;
-        perm(nums, curr, taken, result);
+
+        // set<int> taken;
+        // perm(nums, curr, taken, result);
+        
+        vector<bool> taken(nums.size());
+        perm2(nums, curr, taken, result);
+        
         return result;
     }
 };
