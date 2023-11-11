5a1efe72703db4fe3612650a82bd70f8e74f8491
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-11 11:41:30 +0530

    Time: 638 ms (66.83%) | Memory: 180.5 MB (84.80%) - LeetSync

---
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,78 @@
+typedef struct Node {
+    Node* children[2];
+    Node(){
+        children[0] = NULL;
+        children[1] = NULL;
+    }
+    Node* addIfNotExists(int bit){
+        if(!children[bit])
+            children[bit] = new Node();
+        return children[bit];
+    }
+    bool hasBit(int bit){
+        return children[bit] != NULL;   
+    }
+    Node* getBit(int bit){
+        return children[bit];
+    }
+}Node;
+
+class Trie {
+    private:
+        Node *root;
+    public:
+        Trie(){
+            root = new Node();
+        }
+        void insert(int num){
+            Node *node = root;
+
+            for(int i=31; i>=0; i--){
+                node = node->addIfNotExists((num >> i) & 1);
+            }
+        }
+        int getXorCandidate(int x){
+            Node *node = root;
+            int best = 0;
+            for(int i=31; i>=0; i--){
+                int desired = !((x >> i) & 1);
+                if(node->hasBit(desired)){
+                    node = node->getBit(desired);
+                    best |= desired << i;
+                } else {
+                    node = node->getBit(!desired);
+                    best |= !(desired) << i;
+                }
+            }
+            return best;
+        }
+};
+
+// 0 1 1
+// 1 0 0
+
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie trie;
+        for(int n : nums){
+            trie.insert(n);
+        }
+
+        int mx = 0;
+        for(int n : nums){
+            // printf("%d ^ %d\n", n, n^trie.getXorCandidate(n));
+            mx = max(mx, n^trie.getXorCandidate(n));
+        }
+        return mx;
+
+        // int mx = 0;
+        // int n= nums.size();
+        // for(int i=0; i<n; i++){
+        //     for(int j=i+1; j<n; j++){
+        //         mx = max(mx, nums[i]^nums[j]);
+        //     }
+        // }
+        // return mx;
+    }
+};
