9b27fd35c898472237fc112bf10f9e8e39a226dc
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-11 13:05:56 +0530

    Time: 566 ms (90.12%) | Memory: 274.8 MB (24.78%) - LeetSync

--- 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.cpp
+++ 1826-maximum-xor-with-an-element-from-array/maximum-xor-with-an-element-from-array.cpp
@@ -6,6 +6,9 @@ typedef struct Node {
         children[0] = NULL;
         children[1] = NULL;
     }
+    bool hasChild(int bit){
+        return children[bit] != NULL;
+    }
     Node* getChild(int bit){
         return children[bit];
     }
@@ -29,6 +32,20 @@ class Trie {
             for(int i=31; i>=0; i--){
                 node = node->addIfNotExists(num>>i & 1, num);
             }
+        }
+        int getMaxXorWithoutMax(int num){
+            Node *node = root;
+            int maxXor = 0;
+            for(int i=31; i>=0; i--){
+                int currXorBit = num>>i & 1;
+                if(node->hasChild(1-currXorBit)){
+                    node = node->getChild(1-currXorBit);
+                    maxXor |= 1<<i;
+                }
+                else
+                    node = node->getChild(currXorBit);
+            }
+            return maxXor;
         }
         int getMaxXor(int num, int mx){
             Node *node = root;
@@ -55,12 +72,39 @@ class Solution {
 public:
     vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
         Trie trie;
-        for(int n : nums)
-            trie.insert(n);
-        vector<int> result;
-        for(auto &query : queries){
-            result.push_back(trie.getMaxXor(query[0], query[1]));
+
+        // for(int n : nums)
+        //     trie.insert(n);
+        // vector<int> result;
+        // for(auto &query : queries){
+        //     result.push_back(trie.getMaxXor(query[0], query[1]));
+        // }
+        // return result;
+
+        vector<int> result(queries.size());
+        vector<int> arr = nums;
+        sort(arr.begin(), arr.end());
+
+        vector<pair<int, pair<int,int>>> ques;
+        for(int q=0; q<queries.size(); q++){
+            auto &query = queries[q];
+            ques.push_back({queries[q][1], {queries[q][0], q}});
         }
+        sort(ques.begin(), ques.end());
+        
+        int i = 0;
+        for(auto &query : ques){
+            while(i<arr.size() && arr[i] <= query.first)
+                trie.insert(arr[i++]);
+
+            // No elmeent in trie
+            if(i==0)
+                result[query.second.second] = -1;
+            else
+                result[query.second.second] = trie.getMaxXorWithoutMax(query.second.first);
+            // printf("Process query-%d, mx(%d) => %d\n", query.second.second, query.first, result[query.second.second]);
+        }
+
         return result;
     }
 };
