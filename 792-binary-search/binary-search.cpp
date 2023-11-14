0f89507c0ebfc11faa49ca5cd6f9b65c77907e72
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-14 09:38:36 +0530

    Time: 24 ms (85.31%) | Memory: 27.8 MB (53.85%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
+    int sch(vector<int>& nums, int target, int l, int r){
+        if(l > r){
+            return -1;
+        }
+        int mid = l + (r-l)/2;
+
+        if(nums[mid] == target)
+            return mid;
+            
+        if(nums[mid] < target)
+            return sch(nums, target, mid+1, r);
+        return sch(nums, target, l, mid-1);
+    }
     int search(vector<int>& nums, int target) {
         int l=0,r=nums.size()-1;
-        int mid;
-        while(l<=r){
-            mid = l + (r-l)/2;
-            if(nums[mid] == target)
-                return mid;
-            if(nums[mid] < target)
-                l = mid+1;
-            else
-                r = mid-1;
-        }
-        return -1;
+
+        return sch(nums, target, l, r);
+
+        // int mid;
+        // while(l<=r){
+        //     mid = l + (r-l)/2;
+        //     if(nums[mid] == target)
+        //         return mid;
+        //     if(nums[mid] < target)
+        //         l = mid+1;
+        //     else
+        //         r = mid-1;
+        // }
+        // return -1;
     }
 };
