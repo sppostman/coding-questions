5afeae6cb1be480074bdf8fc61cab566bc790cde
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-22 01:29:32 +0530

    Time: 22 ms (39.96%) | Memory: 22.6 MB (53.48%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n=nums.size();
-        int l=0, r=n-1;
+
+        if(n==1 || nums[0] != nums[1])
+            return nums[0];
+        if(nums[n-1] != nums[n-2])
+            return nums[n-1];
+
+        int l=2, r=n-3;
         while(l<r){
             int mid = l+(r-l)/2;
-            if((mid==0 || nums[mid] != nums[mid-1]) && (mid==n-1 || nums[mid] != nums[mid+1]))
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
                 return nums[mid];
 
-            if(mid+1<n && nums[mid] == nums[mid+1]){
+            if(nums[mid] == nums[mid+1]){
                 if((r-mid+1) & 1)
                     l=mid;
                 else
                     r=mid-1;
             }
-            else if(mid > 0 && nums[mid] == nums[mid-1]){
+            else if(nums[mid] == nums[mid-1]){
                 if((mid-l+1) & 1)
                     r=mid;
                 else
@@ -22,5 +28,26 @@ public:
             }
         }
         return nums[l];
+
+        // int l=0, r=n-1;
+        // while(l<r){
+        //     int mid = l+(r-l)/2;
+        //     if((mid==0 || nums[mid] != nums[mid-1]) && (mid==n-1 || nums[mid] != nums[mid+1]))
+        //         return nums[mid];
+
+        //     if(mid+1<n && nums[mid] == nums[mid+1]){
+        //         if((r-mid+1) & 1)
+        //             l=mid;
+        //         else
+        //             r=mid-1;
+        //     }
+        //     else if(mid > 0 && nums[mid] == nums[mid-1]){
+        //         if((mid-l+1) & 1)
+        //             r=mid;
+        //         else
+        //             l=mid+1;
+        //     }
+        // }
+        // return nums[l];
     }
 };
