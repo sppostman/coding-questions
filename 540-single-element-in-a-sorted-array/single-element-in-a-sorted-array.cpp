26b0be546aeded70f65184ce7b360b04bd3d4587
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-22 01:48:37 +0530

    Time: 19 ms (65.01%) | Memory: 22.8 MB (23.30%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -13,35 +13,30 @@ public:
             int mid = l+(r-l)/2;
             if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
                 return nums[mid];
-
-            if(nums[mid] == nums[mid+1]){
-                if((r-mid+1) & 1)
-                    l=mid;
-                else
-                    r=mid-1;
+            if(
+                ((mid &1) && nums[mid]==nums[mid-1]) ||
+                ((mid&1)==0 && nums[mid] && nums[mid]==nums[mid+1])
+            ){
+                l = mid+1;
+            } else {
+                r = mid-1;
             }
-            else if(nums[mid] == nums[mid-1]){
-                if((mid-l+1) & 1)
-                    r=mid;
-                else
-                    l=mid+1;
-            }
-        }
-        return nums[l];
+        }       
+        return nums[l]; 
 
-        // int l=0, r=n-1;
+        // int l=2, r=n-3;
         // while(l<r){
         //     int mid = l+(r-l)/2;
-        //     if((mid==0 || nums[mid] != nums[mid-1]) && (mid==n-1 || nums[mid] != nums[mid+1]))
+        //     if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
         //         return nums[mid];
 
-        //     if(mid+1<n && nums[mid] == nums[mid+1]){
+        //     if(nums[mid] == nums[mid+1]){
         //         if((r-mid+1) & 1)
         //             l=mid;
         //         else
         //             r=mid-1;
         //     }
-        //     else if(mid > 0 && nums[mid] == nums[mid-1]){
+        //     else if(nums[mid] == nums[mid-1]){
         //         if((mid-l+1) & 1)
         //             r=mid;
         //         else
