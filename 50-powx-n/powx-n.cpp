61b86a4c61f371a6b3ea951c46d6ee585e741cc6
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-26 23:49:56 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (60.26%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -9,12 +9,33 @@ public:
         // }
         // return isNeg ? 1/result : result;
 
+        // 3^10
+        // 3^2^5 = 9^5
+        // 9^4 * 9
+        // 81^2 * 9
+        // 6561^1 * 9
+        // 6561^0 * 6561 * 9
+        
+        // bool isNeg = n<0;
+        // long pow = abs(n);
+        // double ans=1;
+        // while(pow>0){
+        //     if(pow & 1){
+        //         ans *= x;
+        //         pow--;
+        //     } else {
+        //         x *= x;
+        //         pow >>= 1;
+        //     }
+        // }
+        // return isNeg ? 1.0/ans : ans;
 
-        bool isNeg = n<0;
+        bool isNeg = n < 0;
         long pow = abs(n);
         double ans=1;
+
         while(pow>0){
-            if(pow & 1){
+            if(pow&1){
                 ans *= x;
                 pow--;
             } else {
@@ -22,6 +43,7 @@ public:
                 pow >>= 1;
             }
         }
+
         return isNeg ? 1.0/ans : ans;
     }
 };
