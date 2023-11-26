a1d982d9884bfbc4e8c3c67f78f3bc0706de5bc6
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-27 01:39:56 +0530

    Time: 6 ms (58.73%) | Memory: 7.2 MB (90.66%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -5,21 +5,12 @@ public:
         vector<char> targetMapped(256, -1); 
 
         for(int i=0; i<s.size(); i++){
-            if(s[i] == t[i]){
-                if(mapping[s[i]] != -1 && mapping[s[i]] != s[i])
-                    return false;
-                if(targetMapped[t[i]] != -1 && targetMapped[t[i]] != t[i])
-                    return false;
-                mapping[s[i]] = s[i];
-                targetMapped[s[i]] = s[i];
-            } else {
-                if(mapping[s[i]] != t[i] && mapping[s[i]] != -1)
-                    return false;
-                if(targetMapped[t[i]] != s[i] && targetMapped[t[i]] != -1)
-                    return false;
-                mapping[s[i]] = t[i];
-                targetMapped[t[i]] = s[i];
-            }
+            if(mapping[s[i]] != t[i] && mapping[s[i]] != -1)
+                return false;
+            if(targetMapped[t[i]] != s[i] && targetMapped[t[i]] != -1)
+                return false;
+            mapping[s[i]] = t[i];
+            targetMapped[t[i]] = s[i];
         }
 
         return true;
