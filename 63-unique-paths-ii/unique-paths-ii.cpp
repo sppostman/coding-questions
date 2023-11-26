618078432e373e19576f6aafdbae4f47fa853214
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-27 00:40:34 +0530

    Time: 5 ms (39.00%) | Memory: 8.1 MB (45.50%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -22,13 +22,12 @@ public:
         // return gopaths(obstacleGrid,dp,0,0,m,n);
 
         dp[0][0] = 1;
-        
         for(int i=1; i<m; i++)
             dp[i][0] = obstacleGrid[i][0] ? 0 : dp[i-1][0];
-        for(int j=1; j<n; j++)
-            dp[0][j] = obstacleGrid[0][j] ? 0 : dp[0][j-1];
+        // for(int j=1; j<n; j++)
+        //     dp[0][j] = obstacleGrid[0][j] ? 0 : dp[0][j-1];
 
-        for(int i=1; i<m; i++){
+        for(int i=0; i<m; i++){
             for(int j=1; j<n; j++){
                 if(obstacleGrid[i][j]){
                     dp[i][j] = 0;
@@ -39,7 +38,6 @@ public:
                 }
             }
         }
-        
         return dp[m-1][n-1];
     }
 };
