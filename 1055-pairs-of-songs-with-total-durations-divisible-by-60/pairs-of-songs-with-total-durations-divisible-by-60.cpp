53937fbe8fee87b2597472c507b60641a5c0673f
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-27 01:59:44 +0530

    Time: 36 ms (29.46%) | Memory: 28.5 MB (39.70%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -4,11 +4,11 @@ public:
         unordered_map<int, int> encountered;
         int pairs = 0;
         for(int t : time){
-            if(t%60 == 0){
-                pairs += encountered[0];
-            } else {
-                pairs += encountered[60 - t%60];
-            }
+            // if(t%60 == 0){
+            //     pairs += encountered[0];
+            // } else {
+                pairs += encountered[(60 - t%60)%60];
+            // }
             encountered[t%60]++;
         }
         return pairs;
