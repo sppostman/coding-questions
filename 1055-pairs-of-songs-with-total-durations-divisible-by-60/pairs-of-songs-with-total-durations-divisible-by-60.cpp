25ffdfe619c2fd80d3e7239cdd53cba4ff8d7581
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-27 01:58:25 +0530

    Time: 29 ms (60.11%) | Memory: 28.5 MB (69.07%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -8,11 +8,7 @@ public:
                 pairs += encountered[0];
                 encountered[0]++;
             } else {
-                printf("Search for %d, ", 60-(t%60));
-                if(encountered.find(60 - (t%60)) != encountered.end()){
-                    pairs += encountered[60 - t%60];
-                    printf("found: %d\n", encountered[60 - t%60]);
-                }
+                pairs += encountered[60 - t%60];
                 encountered[t%60]++;
             }
         }
