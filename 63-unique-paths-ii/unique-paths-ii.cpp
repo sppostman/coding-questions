c479b439792f269c526a329ee888005b44581c62
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-27 00:39:36 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (23.17%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -22,18 +22,22 @@ public:
         // return gopaths(obstacleGrid,dp,0,0,m,n);
 
         dp[0][0] = 1;
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+        
+        for(int i=1; i<m; i++)
+            dp[i][0] = obstacleGrid[i][0] ? 0 : dp[i-1][0];
+        for(int j=1; j<n; j++)
+            dp[0][j] = obstacleGrid[0][j] ? 0 : dp[0][j-1];
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
                 if(obstacleGrid[i][j]){
                     dp[i][j] = 0;
-                } else if(!(i==0 && j==0)){
+                } else {
                     int leftPossible = i>0 ? dp[i-1][j] : 0;
                     int rightPossible = j>0 ? dp[i][j-1] : 0;
                     dp[i][j] = leftPossible + rightPossible;
                 }
-                printf("%d, ", dp[i][j]);
             }
-            printf("\n");
         }
         
         return dp[m-1][n-1];
