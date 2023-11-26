d8f6cb3199aff57a267a4eb001180565e7d6ea21
sppostman <67599966+sppostman@users.noreply.github.com>
2023-11-27 02:00:13 +0530

    Time: 29 ms (60.11%) | Memory: 28.5 MB (39.70%) - LeetSync

--- 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1055-pairs-of-songs-with-total-durations-divisible-by-60/pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -3,12 +3,20 @@ public:
     int numPairsDivisibleBy60(vector<int>& time) {
         unordered_map<int, int> encountered;
         int pairs = 0;
+        // for(int t : time){
+        //     if(t%60 == 0){
+        //         pairs += encountered[0];
+        //     } else {
+        //         pairs += encountered[60 - t%60];
+        //     }
+        //     encountered[t%60]++;
+        // }
         for(int t : time){
-            // if(t%60 == 0){
-            //     pairs += encountered[0];
-            // } else {
+            if(t%60 == 0){
+                pairs += encountered[0];
+            } else {
                 pairs += encountered[(60 - t%60)%60];
-            // }
+            }
             encountered[t%60]++;
         }
         return pairs;
