e6090c6ad495d50e49618169445c05b1203a9696
sppostman <67599966+sppostman@users.noreply.github.com>
2024-01-06 13:48:04 +0530

    Time: 11 ms (18.85%) | Memory: 14.2 MB (17.20%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        // int n=nums.size();
-        // int l=0, r=n-1;
-        // int lb=n;
-        // while(l<=r){
-        //     int mid = l+(r-l)/2;
-        //     if(nums[mid] >= target){
-        //         lb = mid;
-        //         r=mid-1;
-        //     } else
-        //         l = mid+1;
-        // }
+        int n=nums.size();
+        int l=0, r=n-1;
+        int lb=n;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(nums[m] >= target){
+                lb=m;
+                r=m-1;
+            } else {
+                l=m+1;
+            }
+        }
+        if(lb==n || nums[lb] != target)
+            return {-1, -1};
 
-        // if(lb == n || nums[lb] != target)
-        //     return {-1,-1};
-
-        // int ub=n;
-        // l=0, r=n-1;
-        // while(l<=r){
-        //     int mid = l+(r-l)/2;
-        //     if(nums[mid] > target){
-        //         ub = mid;
-        //         r=mid-1;
-        //     } else
-        //         l=mid+1;
-        // }
-        // return {lb, ub-1};
+        l=0;
+        r=n-1;
+        int ub=n;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(nums[m] <= target){
+                l=m+1;
+            } else {
+                ub=m;
+                r=m-1;
+            }
+        }
+        return {lb, ub-1};
         
-        int st = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
-        if(st == nums.size() || nums[st] != target)
-            return {-1,-1};
-        int end = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
-        return { st, end-1 };
+        // int st = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        // if(st == nums.size() || nums[st] != target)
+        //     return {-1,-1};
+        // int end = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+        // return { st, end-1 };
     }
 
 };
