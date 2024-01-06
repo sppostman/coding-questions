f496e10136b3877c15a66d4536dc6a39c468064d
sppostman <67599966+sppostman@users.noreply.github.com>
2024-01-06 13:38:51 +0530

    Time: 3 ms (93.68%) | Memory: 14.2 MB (17.20%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,33 +1,38 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int st = lb(nums,target,0,nums.size()-1);
+        // int n=nums.size();
+        // int l=0, r=n-1;
+        // int lb=n;
+        // while(l<=r){
+        //     int mid = l+(r-l)/2;
+        //     if(nums[mid] >= target){
+        //         lb = mid;
+        //         r=mid-1;
+        //     } else
+        //         l = mid+1;
+        // }
+
+        // if(lb == n || nums[lb] != target)
+        //     return {-1,-1};
+
+        // int ub=n;
+        // l=0, r=n-1;
+        // while(l<=r){
+        //     int mid = l+(r-l)/2;
+        //     if(nums[mid] > target){
+        //         ub = mid;
+        //         r=mid-1;
+        //     } else
+        //         l=mid+1;
+        // }
+        // return {lb, ub-1};
+        
+        int st = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
         if(st == nums.size() || nums[st] != target)
             return {-1,-1};
-        return { st, ub(nums,target,0,nums.size()-1)-1 };
+        int end = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+        return { st, end-1 };
     }
-    int lb(vector<int>& nums, int x, int l, int r){
-        int ans=nums.size();
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(nums[mid] >= x){
-                ans=mid;
-                r=mid-1;
-            } else
-                l=mid+1;
-        }
-        return ans;
-    }
-    int ub(vector<int>& nums, int x, int l, int r){
-        int ans=nums.size();
-        while(l<=r){
-            int mid = l+(r-l)/2;
-            if(nums[mid] > x){
-                ans=mid;
-                r=mid-1;
-            } else
-                l=mid+1;
-        }
-        return ans;
-    }
+
 };
