4976b59f3b95f53053cf6c8090ff9b0a6d6ca2b4
sppostman <67599966+sppostman@users.noreply.github.com>
2024-02-10 12:34:52 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (14.99%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -4,10 +4,22 @@ public:
         if(x<2)
             return x;
         
-        long i=1;
-        while(i*i <= x){
-            i++;
+        // long i=1;
+        // while(i*i <= x){
+        //     i++;
+        // }
+        // return i-1;
+
+        long l=1, r=x/2, mid;
+        long ans=1;
+        while(l<=r){
+            mid = l+(r-l)/2;
+            if(mid*mid <= x){
+                ans = mid;
+                l = mid+1;
+            } else
+                r = mid-1;
         }
-        return i-1;
+        return ans;
     }
 };
