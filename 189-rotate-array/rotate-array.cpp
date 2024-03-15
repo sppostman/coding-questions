35c3ffc98776643944a814d3e9f951295cd48af5
sppostman <67599966+sppostman@users.noreply.github.com>
2024-03-15 23:19:19 +0530

    Time: 26 ms (17.78%) | Memory: 27.2 MB (75.22%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -3,17 +3,21 @@ public:
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
         k=k%n;
-        
-        vector<int> extras;
-        for(int i=0; i<n-k; i++){
-            extras.push_back(nums[i]);
-        }
 
-        for(int i=0; i<k; i++)
-            nums[i] = nums[n-k+i];
-        for(int i=k; i<n; i++){
-            nums[i] = extras[i-k];
-        }
+        reverse(nums.begin(), nums.begin()+n-k);
+        reverse(nums.begin()+n-k, nums.end());
+        reverse(nums.begin(), nums.end());
+        
+        // vector<int> extras;
+        // for(int i=0; i<n-k; i++){
+        //     extras.push_back(nums[i]);
+        // }
+
+        // for(int i=0; i<k; i++)
+        //     nums[i] = nums[n-k+i];
+        // for(int i=k; i<n; i++){
+        //     nums[i] = extras[i-k];
+        // }
 
         // int i = 0;
         // while(i<k){
@@ -22,6 +26,5 @@ public:
         // }
         // for(int e : extras)
         //     nums[i++] = e;
-
     }
 };
