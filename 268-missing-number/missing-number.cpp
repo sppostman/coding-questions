8b76f1c2abc4d36859edff05fea6a5177f3f626c
sppostman <67599966+sppostman@users.noreply.github.com>
2024-03-15 23:50:44 +0530

    Time: 15 ms (50.93%) | Memory: 20.4 MB (58.33%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -2,10 +2,19 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        long long expected = (long)n*(n+1)/2;
-        long long actual = 0;
+
+        // long long expected = (long)n*(n+1)/2;
+        // long long actual = 0;
+        // for(int i : nums)
+        //     actual += i;
+        // return expected-actual;
+
+        int xor_expected = 0;
+        int xor_actual = 0;
         for(int i : nums)
-            actual += i;
-        return expected-actual;
+            xor_actual ^= i;
+        for(int i=0; i<=n; i++)
+            xor_expected ^= i;
+        return xor_actual^xor_expected;
     }
 };
