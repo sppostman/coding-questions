db7eff1ba32c66483409db20235a9d88f9e08dd3
sppostman <67599966+sppostman@users.noreply.github.com>
2024-03-15 23:26:40 +0530

    Time: 11 ms (91.46%) | Memory: 21.6 MB (68.01%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int n=nums.size();
+        int anchor = 0, i=0;
+        while(i<n){
+            nums[anchor] = nums[i];
+            if(nums[i] != 0)
+                anchor++;
+            i++;
+        }
+        while(anchor<n)
+            nums[anchor++]=0;
+
+        // int z=0;
+        // printf("n %d\n", n);
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]==0){
+        //         z++;
+        //         // printf("skip %d\n", nums[i]);
+        //         continue;
+        //     }
+        //     // printf("z:%d, nums[%d] <- nums[%d] (%d) | now, n-z: %d\n", z, i-z, i, nums[i], n-z);
+        //     nums[i-z] = nums[i];
+
+
+        //     // if(i-z != i && i+1 >= n-z){
+        //     //     printf("set to 0 %d[%d]\n", nums[i],i);
+        //     //     nums[i]=0;
+        //     // }
+        // }
+
+
+        // // printf("Zs: %d\n",z);
+        // while(z-- > 0){
+        //     nums[n-1-z] = 0;
+        // }
+    }
+};
