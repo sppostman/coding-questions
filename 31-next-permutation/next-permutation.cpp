823363ba35d377e48c838d17cb15a188a8e75ba8
sppostman <67599966+sppostman@users.noreply.github.com>
2024-03-24 10:23:10 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (88.19%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-     void nextPermutation(vector<int>& nums) {
+    void nextPermutation(vector<int>& nums) {
         int n= nums.size();
         int border = -1;
         for(int i=n-1; i>0; i--){
