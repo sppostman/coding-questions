6d5c54303a91f79561243e9e54610e46d14749bf
sppostman <67599966+sppostman@users.noreply.github.com>
2024-03-24 12:14:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (90.71%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -4,11 +4,7 @@ public:
         int n = matrix.size();
         
         /*
-        1 2 3
-        4 5 6
-        7 8 9
-
-        / Transpose & then reverse row-wiser
+        Transpose & then reverse row-wiser
 
         1 4 7  1 4 7
         2 5    2 5 8
@@ -19,28 +15,10 @@ public:
         for(int i=0; i<n; i++)
             for(int j=0; j<i; j++)
                 swap(matrix[i][j], matrix[j][i]);
+
+        // Reverse horizontally
         for(int i=0; i<n; i++)
             for(int j=0; j<n/2; j++)
                 swap(matrix[i][j], matrix[i][n-j-1]);
-
-        // // Transpose
-        // for(int i=0; i<n; i++)
-        //     for(int j=0; j<i; j++)
-        //         swap(matrix[i][j], matrix[j][i]);
-        // for(int i=0; i<n; i++)
-        //     for(int j=0; j<n/2; j++)
-        //         swap(matrix[i][j], matrix[i][n-j-1]);
-
-        // // Brute using extra space
-        // auto res = matrix;
-        
-        // for(int i=0; i<n; i++){
-        //     for(int j=0; j<n; j++){
-        //         res[j][n-i-1] = matrix[i][j];
-        //     }
-        // }
-        // for(int i=0; i<n; i++)
-        //     for(int j=0; j<n; j++)
-        //         matrix[i][j] = res[i][j];
     }
 };
