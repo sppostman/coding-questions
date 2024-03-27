9a0915ff40047abfe976d8e6e28976e57cf568da
sppostman <67599966+sppostman@users.noreply.github.com>
2024-03-28 01:14:30 +0530

    Time: 42 ms (54.79%) | Memory: 57.9 MB (49.98%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -2,25 +2,44 @@ class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
         int n=nums.size();
-        int l=0, r=0;
-        int mx=0, canskip=k;
-
-        queue<int> lastZ;
-
+        int l=0, r=0, skipped=0;
+        int mx = 0;
         while(r<n){
             int el = nums[r];
             if(el == 0){
-                lastZ.push(r);
-                if(canskip == 0){
-                    l = lastZ.front()+1;
-                    lastZ.pop();
-                } else {
-                    canskip--;
+                if(skipped<k)
+                    skipped++;
+                else {
+                    while(nums[l] != 0)
+                        l++;
+                    l++;
                 }
             }
             mx = max(mx, r-l+1);
             r++;
         }
         return mx;
+
+        // // O(n) space for queue
+        // int l=0, r=0;
+        // int mx=0, canskip=k;
+
+        // queue<int> lastZ;
+
+        // while(r<n){
+        //     int el = nums[r];
+        //     if(el == 0){
+        //         lastZ.push(r);
+        //         if(canskip == 0){
+        //             l = lastZ.front()+1;
+        //             lastZ.pop();
+        //         } else {
+        //             canskip--;
+        //         }
+        //     }
+        //     mx = max(mx, r-l+1);
+        //     r++;
+        // }
+        // return mx;
     }
 };
