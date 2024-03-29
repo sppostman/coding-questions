9ae1d60da48ebe18f8764a5c7f5fee5ec9e85cb1
sppostman <67599966+sppostman@users.noreply.github.com>
2024-03-29 15:26:14 +0530

    Time: 111 ms (54.57%) | Memory: 49.7 MB (56.63%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return subLessK(nums, k) - subLessK(nums,k-1);
+        // // O(n^2)
+        // int n=nums.size(), count=0;
+        // for(int i=0; i<n; i++){
+        //     unordered_map<int, int> freq;
+        //     for(int j=i; j<n; j++){
+        //         freq[nums[j]]++;
+        //         if(freq.size() == k)
+        //             count++;
+        //         else if(freq.size() > k)
+        //             break;
+        //     }
+        // }
+        // return count;
+    }
+    int subLessK(vector<int>& nums, int k) {
+        int i=0, j=0, n=nums.size();
+        int count = 0;
+        unordered_map<int, int> freq;
+
+        while(j<n){
+            freq[nums[j]]++;
+
+            while(freq.size() > k){
+                if(--freq[nums[i]] == 0)    
+                    freq.erase(nums[i]);
+                i++;
+            }
+
+            if(freq.size() <= k)
+                count += j-i+1;
+
+            j++;
+        }
+
+
+        return count;
+    }
+};
