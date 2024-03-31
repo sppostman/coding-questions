f0a050639cb22eed245e58be845b088f72445164
sppostman <67599966+sppostman@users.noreply.github.com>
2024-03-31 12:38:48 +0530

    Time: 4 ms (55.21%) | Memory: 13.4 MB (93.65%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        // buy states => 0-sell, 1-cooldown, 2-buy
+        vector<int> ahead(3, 0);
+        vector<int> dp(3, 0);
+
+        for(int i=n-1; i>=0; i--){
+            dp[0] = max(ahead[0], prices[i]+ahead[1]);
+            dp[1] = ahead[2];
+            dp[2] = max(ahead[2], -prices[i]+ahead[0]);
+            ahead = dp;
+        }
+        return max(ahead[1], ahead[2]);
+    }
+};
