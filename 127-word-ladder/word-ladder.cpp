8b631a8f3d611c6a5ab5fc9c53cbfd455b36f478
sppostman <67599966+sppostman@users.noreply.github.com>
2024-04-21 13:47:18 +0530

    Time: 190 ms (47.69%) | Memory: 26.7 MB (26.04%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -12,6 +12,19 @@ public:
         }
         return !allowDiff;
     }
+    void prepareAdjList(string &a, int aidx, unordered_map<string, int> &strs, vector<vector<int>> &adjList){
+        for(int i=0; i<a.size(); i++){
+            char og = a[i];
+            for(char j='a'; j<='z'; j++){
+                a[i] = j;
+
+                if(a[i] != og && strs.find(a) != strs.end()){
+                    adjList[aidx].push_back(strs[a]);
+                }
+            }
+            a[i] = og;
+        }
+    }
     int ladderLength(string beginWord, string endWord, vector<string>& givenWordList) {
         vector<string> wordList = givenWordList;
         wordList.push_back(beginWord);
@@ -21,16 +34,25 @@ public:
 
         int target = -1;
 
+        unordered_map<string,int> strs;
         for(int i=0; i<wordList.size(); i++){
             if(wordList[i] == endWord)
                 target = i;
-            for(int j=i; j<wordList.size(); j++){
-                if(areAdj(wordList[i], wordList[j])){
-                    adjList[i].push_back(j);
-                    adjList[j].push_back(i);
-                }
-            }
+            strs[wordList[i]] = i;
         }
+        for(int i=0; i<wordList.size(); i++){
+            prepareAdjList(wordList[i], i, strs, adjList);
+        }
+        // for(int i=0; i<wordList.size(); i++){
+        //     if(wordList[i] == endWord)
+        //         target = i;
+        //     for(int j=i; j<wordList.size(); j++){
+        //         if(areAdj(wordList[i], wordList[j])){
+        //             adjList[i].push_back(j);
+        //             adjList[j].push_back(i);
+        //         }
+        //     }
+        // }
 
         if(target == -1)
             return 0;
@@ -52,7 +74,7 @@ public:
 
                 if(x == target)
                     return currDist;
-                    
+
                 for(auto y : adjList[x])
                     if(!vis[y]){
                         vis[y] = true;
