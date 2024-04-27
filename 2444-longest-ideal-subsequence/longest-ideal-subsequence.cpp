25a0c54d7826a51bbb1bb8c9c1bae75eef74cad6
sppostman <67599966+sppostman@users.noreply.github.com>
2024-04-27 12:56:47 +0530

    Time: 39 ms (78.28%) | Memory: 11.5 MB (75.10%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -16,49 +16,20 @@ public:
     int longestIdealString(string s, int k) {
         int n = s.size();
 
-        vector<vector<int>> dp(n, vector<int>(27, -1));
-        return solve(0, '{', s, k, dp);
+        // vector<vector<int>> dp(n, vector<int>(27, -1));
+        // return solve(0, '{', s, k, dp);
 
+        vector<int> dp(27, 0);
+        int maxRes = 0;
         for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                dp[i][j] = max(
-                    i > 0 ? dp[i-1][j] : 0,
-                    j > 0 ? dp[i][j-1] : 0
-                );
-                if(i>0 && j>0){
-                    if(abs(s[j-1] - s[i]) <= k)
-                        dp[i][j] = max(dp[i][j], dp[i-1][j-1]+1);
-                } else if(s[j]==s[i])
-                    dp[i][j] = 1;
+            int curr = s[i]-'a';
+            int maxPrev = 0;
+            for(int prev=max(0, curr-k); prev<=min(25, curr+k); prev++){
+                maxPrev = max(maxPrev, dp[prev]);
             }
+            dp[curr] = maxPrev+1;
+            maxRes = max(dp[curr], maxRes);
         }
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++)
-                cout<<dp[i][j]<<" ";
-            cout<<endl;
-        }
-
-        return dp[n-1][n-1];
-
-
-//         vector<int> occured(26, 0);
-//         int longest = 0;
-//         for(char c : s){
-//             int i = c-'a';
-//             int currSeq = 0;
-
-//             occured[i]++;
-
-//             for(int j=max(0, i-k); j<=min(25, i+k); j++){
-//                 if(occured[j])
-//                     printf(" -- take %c => %d\n", j+'a', occured[j]);
-//                 currSeq += occured[j];
-//             }
-// // lkkcg
-// // px
-//             printf("for %c => %d\n", c, currSeq);
-//             longest = max(longest, currSeq);
-//         }
-//         return longest;
+        return maxRes;
     }
 };
