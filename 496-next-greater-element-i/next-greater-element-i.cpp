af6ef85870e18903e21d467a2ed9b6b29c1cb477
sppostman <67599966+sppostman@users.noreply.github.com>
2024-05-08 21:02:40 +0530

    Time: 5 ms (44.65%) | Memory: 12.1 MB (43.94%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -4,6 +4,27 @@ public:
         int m = nums1.size();
         int n = nums2.size();
 
+        stack<int> gt;
+        unordered_map<int, int> nge;
+
+        for(int i=n-1; i>=0; i--){
+            int x = nums2[i];
+            while(gt.size() && gt.top() <= x)
+                gt.pop();
+            nge[x] = gt.empty() ? -1 : gt.top();
+            gt.push(x);
+        }
+
+        vector<int> res(m, -1);
+        for(int i=0; i<m; i++){
+            int x = nums1[i];
+            if(nge.find(x) != nge.end())
+                res[i] = nge[x];
+        }
+        return res;
+
+
+
         // vector<int> nge(n, -1);
         // stack<int> gt;
         // unordered_map<int,int> pos;
@@ -25,26 +46,5 @@ public:
         //     }
         // }
         // return res;
-
-
-        stack<int> gt;
-        unordered_map<int,int> nge;
-        for(int i=n-1; i>=0; i--){
-            int el = nums2[i];
-            while(gt.size() && gt.top()<el)
-                gt.pop();
-            if(gt.size())
-                nge[el] = gt.top();
-            gt.push(el);
-        }
-
-        vector<int> res(m, -1);
-        for(int i=0; i<m; i++){
-            int el = nums1[i];
-            if(nge.find(el) != nge.end()){
-                res[i] = nge[el];
-            }
-        }
-        return res;
     }
 };
