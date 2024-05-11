557d1d0febf9bf7bebec965d0a0550e04b0f0a3d
sppostman <67599966+sppostman@users.noreply.github.com>
2024-05-11 11:54:18 +0530

    Time: 10 ms (85.33%) | Memory: 13.1 MB (50.68%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -57,8 +57,37 @@ public:
         return sum;
     }
 
+    long long sumSubArrayCmp(vector<int> &nums, int n, bool min){
+        // 3 1 2 4 => 17
+
+        stack<int> pce;
+        long long sum = 0;
+        long long left,right;
+
+        for(int i=0; i<n; i++){
+            while(pce.size() && (min ? nums[pce.top()] > nums[i] : nums[pce.top()] < nums[i])){
+                int j = pce.top();
+                pce.pop();
+
+                left = pce.size() ? j-pce.top() : j+1;
+                right = i-j;
+                sum += nums[j] * left * right;
+            }
+            pce.push(i);
+        }
+        while(pce.size()){
+            int j = pce.top();
+            pce.pop();
+
+            left = pce.size() ? j-pce.top() : j+1;
+            right = n-j;
+            sum += nums[j] * left * right;
+        }
+        return sum;
+    }
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
-        return sumSubArrayMax(nums, n) - sumSubArrayMin(nums, n);
+        return sumSubArrayCmp(nums, n, false) - sumSubArrayCmp(nums, n, true);
+        // return sumSubArrayMax(nums, n) - sumSubArrayMin(nums, n);
     }
 };
