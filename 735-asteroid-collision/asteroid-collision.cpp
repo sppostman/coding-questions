66c10c2cfbad9678155d5ada403c07a560b65962
sppostman <67599966+sppostman@users.noreply.github.com>
2024-05-11 11:05:33 +0530

    Time: 9 ms (60.23%) | Memory: 20.4 MB (67.14%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> rights;
+        vector<int> safe;
+
+        for(int ast : asteroids){
+            if(ast >= 0){
+                rights.push(ast);
+            } else {
+                int size = -ast;
+                while(rights.size() && rights.top() < size)
+                    rights.pop();
+                if(rights.empty())
+                    safe.push_back(ast);
+                else if(rights.top() == size)
+                    rights.pop();
+            }
+        }
+
+        safe.resize(safe.size() + rights.size());
+        int placeAt = safe.size()-1;
+        while(rights.size()){
+            safe[placeAt--] = rights.top();
+            rights.pop();
+        }
+
+        return safe;
+    }
+};
