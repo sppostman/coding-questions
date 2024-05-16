afcfefe3b8a21448fa433b9d8711eeb7e2b85d36
sppostman <67599966+sppostman@users.noreply.github.com>
2024-05-16 12:50:34 +0530

    Time: 20 ms (77.00%) | Memory: 31.1 MB (65.17%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,34 +1,29 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& a, int k) {
-        // Time O(2n)    Space O(n)
-        // int n = a.size();
-        // vector<int> ps(k+n+1,0); // Better than map, since we know upper limit of sum
-        // int sum = 0;
-        // int count = 0;
-        // for(int i=0; i<n; i++){
-        //     sum += a[i];
-        //     if(sum == k)
-        //         count++;
-        //     if(sum-k >= 0)
-        //         count+=ps[sum-k];
-        //     ps[sum]++;
-        // }
+        // Time O(n)    Space O(n)
+        int n = a.size();
         // return count;
 
         // O(4n) ie. 2xO(2n)   Space O(1)
         return numSubarraysWithLessThanEqSum(a, k) - numSubarraysWithLessThanEqSum(a, k-1);
     }
     int numSubarraysWithLessThanEqSum(vector<int>& a, int k){
-        int n = a.size();
-        if(k<0) return 0;
+        if(k<0)
+            return 0;
 
+        int n = a.size();
+        
         int i=0, j=0;
-        int count=0, sum=0;
+        int sum = 0;
+        int count = 0;
+
         while(j<n){
             sum += a[j];
-            while(sum > k)
-                sum -= a[i++];
+            while(sum > k){
+                sum -= a[i];
+                i++;
+            }
             count += j-i+1;
             j++;
         }
