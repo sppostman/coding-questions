d9726aeafa4bd7ed402a5b38402603f4a116a9f5
sppostman <67599966+sppostman@users.noreply.github.com>
2024-05-16 11:44:02 +0530

    Time: 43 ms (47.25%) | Memory: 57.8 MB (48.50%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -1,70 +1,19 @@
 class Solution {
 public:
     int longestOnes(vector<int>& nums, int k) {
-        // longest subarray with at most k zeros ?
-        int n=nums.size();
-        int l=0, r=0, zeros=0;
-        int mx=0;
-        while(r<n){
-            zeros += nums[r]==0;
-
-            if(zeros > k){
-                zeros -= nums[l]==0;
-                l++;
+        int n = nums.size();
+        int i=0, j=0;
+        int flipped = 0;
+        int mxOnes = 0;
+        while(j<n){
+            while(nums[j]==0 && flipped == k){
+                flipped -= nums[i] == 0;
+                i++;
             }
-            if(zeros <= k)
-                mx = max(mx, r-l+1);
-            r++;
-
-            // while(zeros > k){
-            //     zeros -= nums[l]==0;
-            //     l++;
-            // }
-            // mx = max(mx, r-l+1);
-            // r++;
+            flipped += nums[j]==0;
+            mxOnes = max(mxOnes, j-i+1);
+            j++;
         }
-        return mx;
-
-        // // O(1) space
-        // int n=nums.size();
-        // int l=0, r=0, skipped=0;
-        // int mx = 0;
-        // while(r<n){
-        //     int el = nums[r];
-        //     if(el == 0){
-        //         if(skipped<k)
-        //             skipped++;
-        //         else {
-        //             while(nums[l] != 0)
-        //                 l++;
-        //             l++;
-        //         }
-        //     }
-        //     mx = max(mx, r-l+1);
-        //     r++;
-        // }
-        // return mx;
-
-        // // O(n) space for queue
-        // int l=0, r=0;
-        // int mx=0, canskip=k;
-
-        // queue<int> lastZ;
-
-        // while(r<n){
-        //     int el = nums[r];
-        //     if(el == 0){
-        //         lastZ.push(r);
-        //         if(canskip == 0){
-        //             l = lastZ.front()+1;
-        //             lastZ.pop();
-        //         } else {
-        //             canskip--;
-        //         }
-        //     }
-        //     mx = max(mx, r-l+1);
-        //     r++;
-        // }
-        // return mx;
+        return mxOnes;
     }
 };
