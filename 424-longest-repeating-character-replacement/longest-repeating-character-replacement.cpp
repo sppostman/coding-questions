9c08564a5b3ab4f994c6d30ad29c28b117ad644d
sppostman <67599966+sppostman@users.noreply.github.com>
2024-05-16 12:28:01 +0530

    Time: 6 ms (79.32%) | Memory: 8.1 MB (79.89%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.cpp
@@ -1,60 +1,29 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int n= s.size();
-        int best = 0;
-
-        vector<int> freq(26, 0);
-        int mxFreq=0;
-
+        int n = s.size();
         int i=0, j=0;
-        while(j<n){
-            int c = s[j]-'A';
-            freq[c]++;
-            mxFreq = max(mxFreq, freq[c]);
+        int maxLen = 0;
 
-            // while(j-i+1 - mxFreq > k){
-            //     char rem = s[i]-'A';
-            //     freq[rem]--;
-            //     if(freq[rem]+1 == mxFreq){
-            //         mxFreq = 0;
-            //         for(int k=0; k<26; k++)
-            //             mxFreq = max(mxFreq, freq[k]);
-            //     }
-            //     i++;
-            // }
-            if(j-i+1 - mxFreq > k){
-                char rem = s[i]-'A';
-                freq[rem]--;
-                // // Not required, since lesser mxFreq would dnever contribute to answer
-                // if(freq[rem]+1 == mxFreq){
-                //     mxFreq = 0;
-                //     for(int k=0; k<26; k++)
-                //         mxFreq = max(mxFreq, freq[k]);
-                // }
+        vector<int> freq(26,0);
+        int maxFreq = 0;
+        while(j<n){
+            int c = s[j] - 'A';
+            freq[c]++;
+            maxFreq = max(maxFreq, freq[c]);
+
+            while(j-i+1 - maxFreq > k){
+                freq[s[i]-'A']--;
                 i++;
+                maxFreq = 0;
+                for(int x=0; x<26; x++)
+                    maxFreq = max(maxFreq, freq[x]);
             }
 
-            if(j-i+1 - mxFreq <= k)
-                best = max(best, j-i+1);
+            maxLen = max(maxLen, j-i+1);
             j++;
         }
-        return best;
 
-        // // O(N^2)
-        // for(int i=0; i<n; i++){
-        //     vector<int> freq(26, 0);
-        //     int mxFreq=0;
-        //     for(int j=i; j<n; j++){
-        //         int c = s[j]-'A';
-        //         freq[c]++;
-        //         mxFreq = max(freq[c], mxFreq);
-        //         if(j-i+1 - mxFreq <= k)
-        //             best = max(best, j-i+1);
-        //         else
-        //             break;
-        //     }
-        // }
-        // return best;
+        return maxLen;
     }
 };
