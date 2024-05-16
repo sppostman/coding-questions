7a50bb6373c8d8963cfe34fe68c79efb3aa0db62
sppostman <67599966+sppostman@users.noreply.github.com>
2024-05-16 11:34:20 +0530

    Time: 5 ms (86.91%) | Memory: 9.2 MB (96.09%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -2,40 +2,22 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int n = s.size();
-        vector<int> lastOccurence(256, -1); // Has better O(1) than hashmap's O(1)
+        vector<bool> present(256, false);
 
-        int l = 0, r=0;
-        int mx = 0;
-        while(r<n){
-            char c = s[r];
-            if(lastOccurence[c] >= l)
-                l = lastOccurence[c]+1;
-            lastOccurence[c] = r;
-            mx = max(mx, r-l+1);
-            r++;
-        }
-        return mx;
-    }
-        
-
-    int lengthOfLongestSubstringSpaceNotOptimal(string s) {
-        int l=0, length = 0, largest = 0;
-        unordered_map<char, int> lastOccurence;
-
-        char ch = 'a';
-        for(int i=0; i<s.length(); i++){
-            ch = s.at(i);
-            if(lastOccurence.find(ch) != lastOccurence.end() && lastOccurence[ch] >= l){
-                int thisLength = i - l;
-                largest = max(thisLength, largest);
-
-                l = lastOccurence[ch] + 1;
-                lastOccurence.erase(ch);
+        int i=0, j=0;
+        int mxLength = 0;
+        while(j<n){
+            while(present[s[j]]){
+                present[s[i]] = false;
+                i++;
             }
-            lastOccurence[ch] = i;
+
+            present[s[j]] = true;
+            mxLength = max(mxLength, j-i+1);
+
+            j++;
         }
-        int thisLength = s.length() - l;
-        largest = max(largest, thisLength);
-        return largest;
+
+        return mxLength;
     }
 };
