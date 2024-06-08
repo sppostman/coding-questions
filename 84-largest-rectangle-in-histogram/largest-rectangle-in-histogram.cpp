a9d128a86e01b7015f85669f93cb6fca9d3714c6
sppostman <67599966+sppostman@users.noreply.github.com>
2024-06-08 22:55:43 +0530

    Time: 191 ms (7.83%) | Memory: 77.8 MB (93.46%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,64 +1,22 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        int n = heights.size();
+        // 1 5 7 10 < 3
+        stack<int> st;
         int maxArea = 0;
+        int n = heights.size();
 
-        stack<int> ps;
-        for(int i=0; i<n; i++){
-            while(ps.size() && heights[ps.top()] >= heights[i]){
-                int j = ps.top();
-                ps.pop();
+        for(int i=0; i<=n; i++){
+            while(st.size()>0 && (i==n || heights[st.top()] >= heights[i])){
+                int height = heights[st.top()];
+                st.pop();
+                int width = st.empty() ? i : i-st.top()-1;
 
-                int width = ps.size() ? i - ps.top() - 1 : i;
-                maxArea = max(maxArea, width * heights[j]);
+                maxArea = max(maxArea, height*width);
             }
-            ps.push(i);
+            st.push(i);
         }
 
-        while(ps.size()){
-            int j = ps.top();
-            ps.pop();
-
-            int width = ps.size() ? n - ps.top() - 1 : n;
-
-            maxArea = max(maxArea, width * heights[j]);
-        }
         return maxArea;
-
-        // vector<int> pse = smaller(heights, n, true);
-        // vector<int> nse = smaller(heights, n, false);
-        
-        // int maxArea = 0;
-        // for(int i=0; i<n; i++){
-        //     int left = i - pse[i];
-        //     int right = nse[i] - i;
-        //     maxArea = max(maxArea, (left+right-1)*heights[i]);
-        // }
-        // return maxArea;
-    }
-
-    vector<int> smaller(vector<int>& heights, int n, bool prev) {
-        vector<int> se(n, 0);
-        stack<int> ls;
-
-        int start = prev ? 0 : n-1;
-        int delta = prev ? 1 : -1;
-        int outofboundSmaller = prev ? -1 : n;
-
-        for(int i=start; prev ? i<n : i>=0 ; i+=delta){
-            while(ls.size() && heights[ls.top()] >= heights[i]){
-                int j = ls.top();
-                ls.pop();
-                se[j] = ls.size() ? ls.top() : outofboundSmaller;
-            }
-            ls.push(i);
-        }
-        while(ls.size()){
-            int j = ls.top();
-            ls.pop();
-            se[j] = ls.size() ? ls.top() : outofboundSmaller;
-        }
-        return se;
     }
 };
