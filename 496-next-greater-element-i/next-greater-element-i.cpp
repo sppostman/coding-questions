0c09d011d188c8a8ab26d77782a40299d241c52b
sppostman <67599966+sppostman@users.noreply.github.com>
2024-06-16 10:55:42 +0530

    Time: 8 ms (32.20%) | Memory: 11.6 MB (55.88%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,50 +1,28 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size();
-        int n = nums2.size();
+        int n1 = nums1.size();
+        int n2 = nums2.size();
 
-        stack<int> gt;
-        unordered_map<int, int> nge;
+        map<int, int> positionsInNums2;
+        for(int i=0; i<n2; i++)
+            positionsInNums2[nums2[i]] = i;
+        
+        vector<int> nge(n2, -1);
+        stack<int> dec;
 
-        for(int i=n-1; i>=0; i--){
-            int x = nums2[i];
-            while(gt.size() && gt.top() <= x)
-                gt.pop();
-            nge[x] = gt.empty() ? -1 : gt.top();
-            gt.push(x);
+        for(int i=n2-1; i>=0; i--){
+            while(dec.size() && dec.top() <= nums2[i]){
+                dec.pop();
+            }
+            nge[i] = dec.size() ? dec.top() : -1;
+            dec.push(nums2[i]);
         }
 
-        vector<int> res(m, -1);
-        for(int i=0; i<m; i++){
-            int x = nums1[i];
-            if(nge.find(x) != nge.end())
-                res[i] = nge[x];
+        vector<int> ngeResult;
+        for(int num : nums1){
+            ngeResult.push_back(nge[positionsInNums2[num]]);
         }
-        return res;
-
-
-
-        // vector<int> nge(n, -1);
-        // stack<int> gt;
-        // unordered_map<int,int> pos;
-        // for(int i=n-1; i>=0; i--){
-        //     int el = nums2[i];
-        //     pos[el] = i;
-        //     while(gt.size() && gt.top()<el)
-        //         gt.pop();
-        //     if(gt.size())
-        //         nge[i] = gt.top();
-        //     gt.push(el);
-        // }
-
-        // vector<int> res(m, -1);
-        // for(int i=0; i<m; i++){
-        //     int el = nums1[i];
-        //     if(pos.find(el) != pos.end()){
-        //         res[i] = nge[pos[el]];
-        //     }
-        // }
-        // return res;
+        return ngeResult;
     }
 };
