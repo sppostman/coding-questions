bedd03386c8e53c35aa04fb40d9a9ac89b285154
sppostman <67599966+sppostman@users.noreply.github.com>
2024-06-16 11:57:04 +0530

    Time: 11 ms (53.87%) | Memory: 22.3 MB (82.09%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -22,4 +22,30 @@ public:
 
         return water;
     }
+    int trapWithStack(vector<int>& height) {
+        int n = height.size();
+        int water = 0;
+
+        stack<int> dec;
+        for(int i=0; i<n; i++){
+            while(dec.size() && height[i] > height[dec.top()]){
+                int maxR = height[i];
+
+                int curr = height[dec.top()];
+                dec.pop();
+
+                if(dec.empty())
+                    break;
+                
+                int maxL = height[dec.top()];
+                
+                int height = min(maxR, maxL) - curr;
+                int width = i - dec.top() - 1;
+                water += height * width;
+            }
+            dec.push(height[i]);
+        }
+
+        return water;
+    }
 };
