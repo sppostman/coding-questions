826e9caa95c73b674fc4bd266079eb0902b22de5
sppostman <67599966+sppostman@users.noreply.github.com>
2024-06-22 12:36:46 +0530

    Time: 17 ms (65.52%) | Memory: 15.9 MB (94.65%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,52 +1,67 @@
 class Solution {
 public:
-    bool bfs(
-        vector<vector<int>>& graph, int i, vector<bool> &vis,
-        vector<bool> &a, vector<bool> &b
-    ){
-        queue<int> rem;
-        rem.push(i);
-        vis[i] = true;
+    // bool bfs(
+    //     vector<vector<int>>& graph, int i, vector<bool> &vis,
+    //     vector<bool> &a, vector<bool> &b
+    // ){
+    //     queue<int> rem;
+    //     rem.push(i);
+    //     vis[i] = true;
 
-        while(rem.size()){
-            int u = rem.front();
-            rem.pop();
+    //     while(rem.size()){
+    //         int u = rem.front();
+    //         rem.pop();
 
-            // 0 (a)    2(a)
-            // 1 3 (b)
+    //         for(int v : graph[u]){
+    //             if(a[u] && a[v]){
+    //                 a[v] = false;
+    //                 b[v] = true;
+    //             } else if(b[u] && b[v]){
+    //                 return false;
+    //             }
+    //             if(!vis[v]){
+    //                 vis[v] = true;
+    //                 rem.push(v);
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
 
-            // q => 2
-            for(int v : graph[u]){
-                if(a[u] && a[v]){
-                    a[v] = false;
-                    b[v] = true;
-                } else if(b[u] && b[v]){
-                    cout<<u<<" "<<v<<endl;
+    bool dfs(int i, int currColor, vector<vector<int>>& graph, vector<int> &color){
+        color[i] = currColor;
+
+        for(int v : graph[i]){
+            if(color[v] == -1){
+                if(!dfs(v, 1-currColor, graph, color))
                     return false;
-                }
-                if(!vis[v]){
-                    vis[v] = true;
-                    rem.push(v);
-                }
-            }
+            } else if(color[i] == color[v])
+                return false;
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<bool> a(n, false);
-        vector<bool> b(n, false);
 
+        vector<int> color(n, -1);
         for(int i=0; i<n; i++)
-            a[i] = true;
-
-        vector<bool> vis(n, false);
-        for(int i=0; i<n; i++){
-            if(!vis[i])
-                if(bfs(graph, i, vis, a, b) == false)
+            if(color[i] == -1)
+                if(!dfs(i, 0, graph, color))
                     return false;
-        }
-        
         return true;
+
+        // vector<bool> a(n, false);
+        // vector<bool> b(n, false);
+
+        // for(int i=0; i<n; i++)
+        //     a[i] = true;
+
+        // vector<bool> vis(n, false);
+        // for(int i=0; i<n; i++){
+        //     if(!vis[i])
+        //         if(bfs(graph, i, vis, a, b) == false)
+        //             return false;
+        // }
+        // return true;
     }
 };
