61c911038aaaecb7741c12c1c11b4258ec028938
sppostman <67599966+sppostman@users.noreply.github.com>
2024-07-20 16:07:01 +0530

    Time: 29 ms (73.26%) | Memory: 21.5 MB (26.35%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    bool works(vector<int>& piles, int k, int hours){
-        for(int pile : piles){
-            hours -= ceil(pile*1.0/k);
-            if(hours<0)
+    bool works(vector<int>& piles, int h, int perhour){
+        int time = 0;
+        for(int b : piles){
+            time += (b+perhour-1)/perhour;
+            if(time > h)
                 return false;
         }
         return true;
     }
-    int minEatingSpeed(vector<int>& piles, int hours) {
-        int l = 1, mid;
-        int h = *max_element(piles.begin(), piles.end());
-        // int h = piles[0];
-        // for(int pile : piles)
-        //     h = max(h, pile);
-        
-        int bestK = h;
-
-        while(l <= h){
-            mid = l+(h-l)/2;
-
-            if(works(piles, mid, hours)){
-                bestK = mid;
-                h = mid-1;
-            } else
-                l = mid+1;
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int l = 1, r = *max_element(piles.begin(), piles.end());
+        int ans = r;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(works(piles, h, m)){
+                r = m-1;
+                ans = m;
+            } else {
+                l = m+1;
+            }
         }
-
-        return bestK;
+        return ans;
     }
 };
