2c2751f7343b98594623994a045fbbd59d32570f
sppostman <67599966+sppostman@users.noreply.github.com>
2024-07-20 16:29:03 +0530

    Time: 105 ms (87.14%) | Memory: 69.4 MB (11.19%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    bool works(vector<int>& bloomDay, int m, int k, int day){
-        int consec = 0;
-        int bouquets = 0;
-        for(int b : bloomDay){
-            if(b-day <= 0){
-                consec++;
+    bool canMake(vector<int>& bloomDay, int m, int k, int day){
+        int bouqets = 0;
+        int takenFlowers = 0;
+        for(int bd : bloomDay){
+            if(bd <= day){
+                takenFlowers++;
+                if(takenFlowers == k){
+                    bouqets++;
+                    takenFlowers = 0;
+                }
             } else {
-                bouquets += consec / k;
-                consec = 0;
-                if(bouquets >= m)
-                    return true;
+                takenFlowers = 0;
             }
         }
-        bouquets += consec / k;
-        return bouquets >= m;
+        return bouqets >= m;
     }
     int minDays(vector<int>& bloomDay, int m, int k) {
-        int l = 1;
-        int h = *max_element(bloomDay.begin(), bloomDay.end());
-        int days=-1;
+        int l = *min_element(bloomDay.begin(), bloomDay.end());
 
-        while(l <= h){
-            int mid = l+(h-l)/2;
+        int mx = *max_element(bloomDay.begin(), bloomDay.end());
+        int r = mx;
 
-            if(works(bloomDay, m, k, mid)){
-                h = mid-1;
-                days = mid;
-            } else
+        int mid;
+        while(l<=r){
+            mid = l+(r-l)/2;
+            if(canMake(bloomDay, m, k, mid)){
+                r = mid-1;
+            } else {
                 l = mid+1;
+            }
         }
-        return days;
+        return l > mx ? -1 : l;
     }
 };
