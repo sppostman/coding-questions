f842c0817a76fff71f798656d77667effb3289d7
sppostman <67599966+sppostman@users.noreply.github.com>
2024-07-24 23:09:53 +0530

    Time: 18 ms (86.80%) | Memory: 94.8 MB (49.16%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums2.size() < nums1.size())
-            return findMedianSortedArrays(nums2, nums1);
-        
         int n1 = nums1.size();
         int n2 = nums2.size();
-        
-        int total = nums1.size() + nums2.size();
-        int half = (total+1)>>1;
-        
-        int l=0, r=min(half, n1);
-        while(l<=r){
-            int el1 = l + ((r-l)>>1);
-            int el2 = half-el1;
+        int n = n1+n2;
 
-            int l1 = el1 > 0 ? nums1[el1-1] : -1e7;
-            int l2 = el2 > 0 ? nums2[el2-1] : -1e7;
-            int r1 = el1 < n1 ? nums1[el1] : 1e7;
-            int r2 = el2 < n2 ? nums2[el2] : 1e7;
+        int i = 0, j = 0;
+        int taken = 0;
+        int goal = (n+1)/2;
+        int goal2 = n%2 == 0 ? goal+1 : goal;
+        int el1, el2;
 
-            printf("l1 %d, r1 %d    l2 %d, r2 %d\n",l1,r1,l2,r2);
-
-            if(l1<=r2 && l2<=r1)
-                return (total&1) == 1 ? max(l1, l2) : (max(l1,l2)+min(r1,r2))/2.0;
-
-            if(l1>r2)
-                r = el1-1;
-            else
-                l = el1+1;
+        while(taken < goal2){
+            taken++;
+            int picked;
+            if(i<n1 && j<n2){
+                if(nums1[i] <= nums2[j]){
+                    picked = nums1[i];
+                    i++;
+                } else {
+                    picked = nums2[j];
+                    j++;
+                }
+            } else {
+                picked = i<n1 ? nums1[i++] : nums2[j++];
+            }
+            if(taken == goal || taken == goal2)
+                cout<<picked<<endl;
+            if(taken == goal)
+                el1 = el2 = picked;
+            if(taken == goal2)
+                el2 = picked;
         }
-        return 0;
+
+        return (el1+el2)*1.0/2;
     }
 };
