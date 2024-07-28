591dfbd7b04dcf95f1ea5a0c16bac9922d77830b
sppostman <67599966+sppostman@users.noreply.github.com>
2024-07-28 11:38:46 +0530

    Time: 84 ms (61.54%) | Memory: 8.3 MB (100.00%) - LeetSync

---
+++ 3507-find-the-count-of-numbers-which-are-not-special/find-the-count-of-numbers-which-are-not-special.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int nonSpecialCount(int l, int r) {
+        int num = sqrt(r);
+        int special = 0;
+
+        while (num * num >= l && num * num <= r) {
+            bool flag = true;
+            for (int i = 2; i <= sqrt(num); i++) {
+                if (num % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                special++;
+            }
+            num--;
+        }
+
+        int ans = r - l + 1 - special;
+        if (l == 1) {
+            ans++;
+        }
+        return ans;
+    }
+};
