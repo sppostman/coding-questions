7f9f8b180f008146cda019d5484a348e7d9baf39
sppostman <67599966+sppostman@users.noreply.github.com>
2024-08-29 23:17:36 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (78.85%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -12,7 +12,17 @@ public:
         return dp[n] = ways;
     }
     int climbStairs(int n) {
-        vector<int> dp(50, -1);
-        return solve(n, dp);
+        // vector<int> dp(50, -1);
+        // return solve(n, dp);
+
+        // 0 1 2 3 4 5
+        // 1 1 2 3 5 8
+        int prev = 1, prev2 = 1;
+        for(int i=2; i<=n; i++){
+            int curr = prev+prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
     }
 };
