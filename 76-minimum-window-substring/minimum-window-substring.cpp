833536da98ae237666ed0da33405e35435ad7453
sppostman <67599966+sppostman@users.noreply.github.com>
2024-08-30 23:25:05 +0530

    Time: 4 ms (94.32%) | Memory: 10.2 MB (73.61%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,81 +1,34 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int n = s.size();
-        int m = t.size();
-        vector<int> tchar(256, 0);
-        for(auto c : t)
-            tchar[c]++;
+        vector<int> tchars(256, 0);
+        for(char c : t)
+            tchars[c]++;
 
-        int i=0, j=0;
-        int available = 0;
-        int minWindow = INT_MAX, minWindowI = -1;
+        int gotten = 0;
+        int k = t.size();
+        int n = s.size();
+
+        int i=0, j=0, minWindow=INT_MAX, minI=-1;
+
         while(j<n){
             char c = s[j];
-            available += tchar[c]>0;
-            tchar[c]--;
+            if(tchars[c] > 0)
+                gotten++;
+            tchars[c]--;
 
-            while(available == m){
+            while(gotten == k){
                 if(j-i+1 < minWindow){
+                    minI = i;
                     minWindow = j-i+1;
-                    minWindowI = i;
                 }
-                tchar[s[i]]++;
-                available -= tchar[s[i]] > 0;
+                tchars[s[i]]++;
+                if(tchars[s[i]] > 0)
+                    gotten--;
                 i++;
             }
-
             j++;
         }
-        return minWindowI == -1 ? "" : s.substr(minWindowI, minWindow);
-
-        // vector<int> tchar(256, 0);
-        // for(auto c : t) tchar[c]++;
-
-        // int best = INT_MAX;
-        // int bestI = -1;
-
-        // int i=0, j=0;
-        // int gotten = 0;
-        // while(j<n){
-        //     char c=s[j];
-        //     if(tchar[c] > 0)
-        //         gotten++;
-        //     tchar[c]--;
-
-        //     while(gotten == t.size()){
-        //         if(j-i+1 < best){
-        //             best = j-i+1;
-        //             bestI = i;
-        //         }
-        //         tchar[s[i]]++;
-        //         if(tchar[s[i]] > 0)
-        //             gotten--;
-        //         i++;
-        //     }
-
-        //     j++;
-        // }
-        // return bestI == -1 ? "" : s.substr(bestI, best);
-
-        // for(int i=0; i<n; i++){
-        //     vector<int> rem = tchar; // Copied
-        //     int gotten = 0;
-        //     for(int j=i; j<n; j++){
-        //         char c = s[j];
-        //         if(rem[c] > 0)
-        //             gotten++;
-        //         rem[c]--;
-                
-        //         if(gotten == t.size()){
-        //             if(j-i+1 < best){
-        //                 best = j-i+1;
-        //                 bestI = i;
-        //             }
-        //             break;
-        //         }
-        //     }
-        // }
-        // return bestI == -1 ? "" : s.substr(bestI, best);
+        return minI == -1 ? "" : s.substr(minI, minWindow);
     }
 };
