0ca0462d31c817036ad83bf7ffddf4199ac11563
sppostman <67599966+sppostman@users.noreply.github.com>
2024-09-02 00:25:02 +0530

    Time: 0 ms (100.00%) | Memory: 13.8 MB (87.83%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,31 +2,24 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-
-        // // buy states => 0-sell, 1-cooldown, 2-buy
-        // vector<int> ahead(3, 0);
-        // vector<int> dp(3, 0);
-
-        // for(int i=n-1; i>=0; i--){
-        //     dp[0] = max(ahead[0], prices[i]+ahead[1]);
-        //     dp[1] = ahead[2];
-        //     dp[2] = max(ahead[2], -prices[i]+ahead[0]);
-        //     ahead = dp;
-        // }
-        // return max(ahead[1], ahead[2]);
-
-
-        // buy states => 0-sell, 1-buy
-        vector<int> nextAhead(2, 0);
-        vector<int> ahead(2, 0);
-        vector<int> dp(2, 0);
+        // dp[i][0] = (dp[i+1][0], -prices[i] + dp[i+2][1])
+        // dp[i][1] = (dp[i+1][1], prices[i] + dp[i+1][0])
+        vector<int> nextNextDp(2, 0);
+        vector<int> nextDp(2, 0);
+        vector<int> currDp(2, 0);
 
         for(int i=n-1; i>=0; i--){
-            dp[0] = max(ahead[0], prices[i]+nextAhead[1]);
-            dp[1] = max(ahead[1], -prices[i]+ahead[0]);
-            nextAhead = ahead;
-            ahead = dp;
+            currDp[0] = max(
+                nextDp[0],
+                prices[i] + nextNextDp[1]
+            );
+            currDp[1] = max(
+                nextDp[1],
+                -prices[i] + nextDp[0]
+            );
+            nextNextDp = nextDp;
+            nextDp = currDp;
         }
-        return ahead[1];
+        return currDp[1];
     }
 };
