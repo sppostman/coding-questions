f7a4fec254891a2711a2b1bab96fcc114346d216
sppostman <67599966+sppostman@users.noreply.github.com>
2024-09-01 23:55:03 +0530

    Time: 7 ms (43.48%) | Memory: 17.2 MB (35.20%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,62 +1,23 @@
 class Solution {
 public:
-    int trade(vector<int>& prices, int buy, int i, int n, vector<vector<int>> &dp){
-        if(i==n)
-            return 0;
-        if(dp[i][buy] != -1)
-            return dp[i][buy];
-
-        int profit = 0;
-
-        if(buy){
-            // Can Buy
-            profit = max(
-                trade(prices, 1, i+1, n, dp),
-                -prices[i] + trade(prices, 0, i+1, n, dp)
-            );
-        } else {
-            // Can Sell
-            profit = max(
-                trade(prices, 0, i+1, n, dp),
-                prices[i] + trade(prices, 1, i+1, n, dp)
-            );
-        }
-        return dp[i][buy] = profit;
-    }
     int maxProfit(vector<int>& prices) {
+        // dp[i][0] => sell
+        // dp[i][1] => buy
         int n = prices.size();
+        vector<int> nextDp(2, 0);
 
-        // vector<vector<int>> dp(n+1, vector<int>(2, -1));
-        // return trade(prices, 1, 0, n, dp);
-
-        // vector<vector<int>> dp(n+1, vector<int>(2, -1));
-        // dp[n][0] = 0;
-        // dp[n][1] = 0;
-        // for(int i=n-1; i>=0; i--){
-        //     dp[i][1] = max(
-        //         dp[i+1][1],
-        //         -prices[i]+dp[i+1][0]
-        //     );
-        //     dp[i][0] = max(
-        //         dp[i+1][0],
-        //         prices[i]+dp[i+1][1]
-        //     );
-        // }
-        // return dp[0][1];
-
-        vector<int> ahead(2, 0);
         for(int i=n-1; i>=0; i--){
             vector<int> dp(2, 0);
             dp[1] = max(
-                ahead[1],
-                -prices[i]+ahead[0]
+                nextDp[1],
+                -prices[i] + nextDp[0]
             );
             dp[0] = max(
-                ahead[0],
-                prices[i]+ahead[1]
+                nextDp[0],
+                prices[i] + nextDp[1]
             );
-            ahead = dp;
+            nextDp = dp;
         }
-        return ahead[1];
+        return nextDp[1];
     }
 };
