f32d8a643cd4699a091cebe947b67ac970220310
sppostman <67599966+sppostman@users.noreply.github.com>
2024-11-08 12:07:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (40.25%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,49 +1,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        // bool isNeg = n<0;
-        // n=abs(n);
-        // double result=1;
-        // while(n-- > 0){
-        //     result *= x;
-        // }
-        // return isNeg ? 1/result : result;
-
-        // 3^10
-        // 3^2^5 = 9^5
-        // 9^4 * 9
-        // 81^2 * 9
-        // 6561^1 * 9
-        // 6561^0 * 6561 * 9
-        
-        // bool isNeg = n<0;
-        // long pow = abs(n);
-        // double ans=1;
-        // while(pow>0){
-        //     if(pow & 1){
-        //         ans *= x;
-        //         pow--;
-        //     } else {
-        //         x *= x;
-        //         pow >>= 1;
-        //     }
-        // }
-        // return isNeg ? 1.0/ans : ans;
-
-        bool isNeg = n < 0;
-        long pow = abs(n);
-        double ans=1;
-
-        while(pow>0){
-            if(pow&1){
-                ans *= x;
-                pow--;
-            } else {
-                x *= x;
-                pow >>= 1;
-            }
+        double result = 1;
+        if(n<0){
+            x = 1/x;
+            n = abs(n);
         }
-
-        return isNeg ? 1.0/ans : ans;
+        while(n>0){
+            if(n&1) result *= x;
+            n >>= 1;
+            x *= x;
+        }
+        return result;
     }
 };
