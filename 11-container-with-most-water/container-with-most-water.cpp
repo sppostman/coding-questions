9bab7f09c6060317ea2e4b232e91cbbf34de8805
sppostman <67599966+sppostman@users.noreply.github.com>
2024-11-11 00:01:40 +0530

    Time: 7 ms (8.41%) | Memory: 61.8 MB (32.38%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -5,11 +5,10 @@ public:
         int l = 0, r = n-1;
         int res = 0;
         while(l<r){
+            res = max(res, (r-l) * min(height[l], height[r]));
             if(height[l] <= height[r]){
-                res = max(res, (r-l) * height[l]);
                 l++;
             } else {
-                res = max(res, (r-l) * height[r]);
                 r--;
             }
         }
