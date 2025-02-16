66eef303fce17623b75814de82f3c0ef7d7aa4c3
sppostman <67599966+sppostman@users.noreply.github.com>
2025-02-16 13:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (13.95%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& mat) {
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        int level = 0;
         vector<int> res;
-        int m=mat.size(), n=mat[0].size();
-        int l=0, r=n-1;
-        int t=0, b=m-1;
-        while(l<=r && t<=b){
-            for(int j=l; j<=r; j++)
-                res.push_back(mat[t][j]);
-            t++;
-
-            for(int i=t; i<=b; i++)
-                res.push_back(mat[i][r]);
-            r--;
-
-            if(t <= b){
-                for(int j=r; j>=l; j--)
-                    res.push_back(mat[b][j]);
-                b--;
-            }
-            if(l <= r){
-                for(int i=b; i>=t; i--)
-                    res.push_back(mat[i][l]);
-                l++;
-            }
+        while(level < ceil(min(m,n)*1.0/2)){
+            for(int j=level; j<n-level; j++)
+                res.push_back(matrix[level][j]);
+            for(int i=level+1; i<m-level; i++)
+                res.push_back(matrix[i][n-1-level]);
+            if(level != m-level-1)
+                for(int j=n-level-2; j>=level; j--)
+                    res.push_back(matrix[m-1-level][j]);
+            if(level != n-level-1)
+                for(int i=m-level-2; i>level; i--)
+                    res.push_back(matrix[i][level]);
+            level++;
         }
         return res;
     }
