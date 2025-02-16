a79fc42bac7eea1ef2ed17999a5dc1d95ba0910f
sppostman <67599966+sppostman@users.noreply.github.com>
2025-02-16 13:07:20 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (33.12%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,28 +1,44 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n= nums.size();
-        int border = -1;
-        for(int i=n-1; i>0; i--){
-            if(nums[i-1] < nums[i]){
-                border = i-1;
-                break;
-            }
-        }
+        /**
+        7 1,3 1
+        7 3 1 1
 
-        if(border == -1){
-            reverse(nums.begin(), nums.end());
+        7 3 8 4 7 1 2 3  ->  ascending, swap last pair
+        7 3 8 4 7 1|3 2 -> border on descending, find upperbound swap with it
+                            sort on right of border
+
+        7 3 2 1
+        
+        7 3 8 4 7 2 1 3 
+        7 3 8 4 7 2 3 1
+        7 3 8 4 7 3 1 2
+
+        7 3 8 4|7 3 2 1 -> descending, swap on border sort right side
+        7 3 8 7 1 2 3 4
+
+        **/
+        int n = nums.size();
+        int border = n-1;
+        while(border>0 && nums[border-1]>=nums[border]){
+            border--;
+        }
+        if(border==0){
+            sort(nums.begin(), nums.end());
             return;
         }
-
-        for(int i=n-1; i>border; i--){
-            if(nums[i] > nums[border]){
-                swap(nums[i], nums[border]);
-                break;
-            }
+        int l = border, r = n-1;
+        int x = nums[border-1];
+        int swapidx = border;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(nums[m] > x){
+                swapidx = m;
+                l = m+1;
+            } else r = m-1;
         }
-
-        reverse(nums.begin()+border+1, nums.end());
-        // 1 2 5 3 4
-    }
+        swap(nums[border-1], nums[swapidx]);
+        sort(nums.begin()+border, nums.end());
+    }   
 };
