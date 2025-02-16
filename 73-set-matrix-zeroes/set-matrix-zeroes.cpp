cc4d3a30be8a45949e5c11649089e7db6775d2f9
sppostman <67599966+sppostman@users.noreply.github.com>
2025-02-16 11:18:11 +0530

    Time: 3 ms (11.56%) | Memory: 18.7 MB (25.11%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,20 +2,32 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
-        set<int> rows, cols;
+        bool rowZ0 = false;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j] == 0){
-                    rows.insert(i);
-                    cols.insert(j);
+                    if(i==0)
+                        rowZ0 = true;
+                    else
+                        matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(rows.count(i) || cols.count(j))
-                    matrix[i][j] = 0;
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                if(i==0)
+                    matrix[i][j] = rowZ0 ? 0 : matrix[i][j];
+                else
+                    matrix[i][j] = matrix[i][0]==0 ? 0 : matrix[i][j];
+                matrix[i][j] = matrix[0][j] == 0 ? 0 : matrix[i][j];
             }
         }
+        if(matrix[0][0] == 0)
+            for(int i=0; i<m; i++)
+                matrix[i][0] = 0;
+        if(rowZ0)
+            for(int j=0; j<n; j++)
+                matrix[0][j] = 0;
     }
 };
