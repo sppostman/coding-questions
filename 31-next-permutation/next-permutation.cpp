444e78091b0017388214fb33391152da29cdbc89
sppostman <67599966+sppostman@users.noreply.github.com>
2025-02-16 13:07:43 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (33.12%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,24 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        /**
-        7 1,3 1
-        7 3 1 1
-
-        7 3 8 4 7 1 2 3  ->  ascending, swap last pair
-        7 3 8 4 7 1|3 2 -> border on descending, find upperbound swap with it
-                            sort on right of border
-
-        7 3 2 1
-        
-        7 3 8 4 7 2 1 3 
-        7 3 8 4 7 2 3 1
-        7 3 8 4 7 3 1 2
-
-        7 3 8 4|7 3 2 1 -> descending, swap on border sort right side
-        7 3 8 7 1 2 3 4
-
-        **/
         int n = nums.size();
         int border = n-1;
         while(border>0 && nums[border-1]>=nums[border]){
