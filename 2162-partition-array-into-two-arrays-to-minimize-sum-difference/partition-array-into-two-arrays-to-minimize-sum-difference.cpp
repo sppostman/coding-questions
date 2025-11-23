273fa4e3f209f7ef58290bddc853cdb7775097bb
sppostman <67599966+sppostman@users.noreply.github.com>
2025-11-23 18:33:12 +0530

    Time: 460 ms (43.17%) | Memory: 186.1 MB (5.20%) - LeetSync

--- 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -11,6 +11,28 @@ public:
         makeSubsets(i+1, arr, n, currSum + arr[i], taken+1, sumWays);
     }
 
+    pair<vector<vector<long long>>, vector<vector<long long>>> makeSubsetsBitMan(vector<int> &arr){
+        int n = arr.size() >> 1;
+        vector<vector<long long>> sumWays1(n+1);
+        vector<vector<long long>> sumWays2(n+1);
+
+        for(int mask = 0; mask < (1<<n); mask++){
+            int sum1 = 0, sum2 = 0;
+            int taken = 0;
+            for(int i=0; i<n; i++){
+                if(mask & (1<<i)){
+                    taken++;
+                    sum1 += arr[i];
+                    sum2 += arr[i+n];
+                }
+            }
+            sumWays1[taken].push_back(sum1);
+            sumWays2[taken].push_back(sum2);
+        }
+
+        return {sumWays1, sumWays2};
+    }
+
     int minimumDifference(vector<int>& arr) {
         int n = arr.size();
         int nhalf = n/2;
@@ -18,9 +40,11 @@ public:
         long long totalSum = accumulate(arr.begin(), arr.end(), 0);
         long long halfSum = totalSum >> 1;
 
-        vector<vector<long long>> sumWays1(nhalf+1), sumWays2(nhalf+1);
-        makeSubsets(0, arr, n/2, 0, 0, sumWays1);
-        makeSubsets(n/2, arr, n, 0, 0, sumWays2);
+        // vector<vector<long long>> sumWays1(nhalf+1), sumWays2(nhalf+1);
+        // makeSubsets(0, arr, n/2, 0, 0, sumWays1);
+        // makeSubsets(n/2, arr, n, 0, 0, sumWays2);
+
+        auto [sumWays1, sumWays2] = makeSubsetsBitMan(arr);
 
         for (auto &p : sumWays2) {
             sort(p.begin(), p.end());
@@ -30,7 +54,6 @@ public:
         for(int taken=0; taken<=nhalf; taken++){
             auto &sumList = sumWays1[taken];
         
-            // cout<<taken<<" - ";
             auto complementary = sumWays2[nhalf - taken];
 
             for(auto sum : sumList){
@@ -40,7 +63,6 @@ public:
                 }
                 
                 long long remSum = halfSum - sum;
-                // cout<<sum<<"-"<<remSum<<", ";
                 auto lb = lower_bound(complementary.begin(), complementary.end(), remSum);
             
                 if(lb != complementary.end())
@@ -55,7 +77,6 @@ public:
                 if(upi > 0)
                     ans = min(ans, abs(2*(sum + complementary[upi-1]) - totalSum));
             }
-            // cout<<endl;
         }
 
         return (int)ans;
